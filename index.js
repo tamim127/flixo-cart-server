71b5432d60faff1a1359e45e1b71804d8cf199d0
MD TAMIUL ALAM <eishatahmedtamim70@gmail.com>
2025-11-25 06:18:07 +0600

    added vercel.json

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express();
 const port = process.env.PORT || 5000
 require("dotenv").config();
 
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    // OPTIONS রিকোয়েস্টের জন্য তৎক্ষণাৎ রেসপন্স দাও
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
+
+
+
 app.use(cors());
 app.use(express.json());
 
